3e5685c6caa4fa146bc6c5b35c98632db53c209f
v.stepura@isoit.ru <v.stepura@isoit.ru>
2021-04-23 13:07:27 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const ansvers = [];
+
+ansvers[0] = confirm('Нажмите "ОК"');
+ansvers[1] = prompt('Сколько вас?', '18');
+ansvers[2] = prompt('Ваше имя?', 'Иван');
+
+console.log(typeof ( ansvers ));
+
+
---
+++ js/types.js
@@ -0,0 +1,33 @@
+'use strict';
+
+let number = 4.6;
+//числа
+console.log(4/0); // infinity
+console.log(-4/0); // -infinity
+console.log('string' * 9); // -Nan
+
+//string
+const persone = `Any persone name`;
+
+//bool
+const boolT = true;
+const boolF = false;
+
+//Null 
+console.log(something);
+
+//undefined
+let und;
+console.log(und);
+
+//объекты
+const obj = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj.name);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp', {}, 6, []]
+console.log(arr[2]);
